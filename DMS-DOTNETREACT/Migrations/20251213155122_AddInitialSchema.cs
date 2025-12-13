266e83f2d242e8c093bd98ccf1f17abe6973d665
AliDanagit1 <alidana321pma@gmail.com>
2025-12-13 18:09:21 +0200

    ali dana backend engineer

---
+++ DMS-DOTNETREACT/Migrations/20251213155122_AddInitialSchema.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DMS_DOTNETREACT.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInitialSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
